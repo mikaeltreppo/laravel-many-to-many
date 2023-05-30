59f45c7316ecc5157da62a7acfb2f80c7c039edf
mikaeltreppo <mikaeltreppo@outlook.com>
2023-05-30 21:18:48 +0200

    Visto da post man

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Project;
+use Illuminate\Http\Request;
+
+class ProjectController extends Controller
+{
+    public function index()
+    {
+        $projects = Project::paginate(9);
+        return response()->json(
+            [
+                'success' => true,
+                'results' => $projects
+
+            ]
+        );
+
+    }
+}
